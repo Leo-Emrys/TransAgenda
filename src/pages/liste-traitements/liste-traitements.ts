aa0812ed9396607007e601f13972ae492380b3c3
ktaxicolor <boris.taxicolor@gmail.com>
2019-08-03 15:52:01 +0200

    Date de la derniere prise avec datepicker

--- src/pages/liste-traitements/liste-traitements.ts
+++ src/pages/liste-traitements/liste-traitements.ts
@@ -1,8 +1,8 @@
-import { Component, NgZone } from '@angular/core';
+import { Component, NgZone, ViewChild } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 import { FonctionsCommunesProvider } from "../../providers/fonctions-communes/fonctions-communes";
-import { GestionTraitementProvider} from "../../providers/gestion-traitement/gestion-traitement"
+import { GestionTraitementProvider } from "../../providers/gestion-traitement/gestion-traitement"
 import { DetailTraitementPage } from '../detail-traitement/detail-traitement';
 import { NewTraitementPage } from '../new-traitement/new-traitement';
 import { NotificationsProvider } from '../../providers/notifications/notifications';
@@ -14,21 +14,24 @@ import { NotificationsProvider } from '../../providers/notifications/notificatio
 export class ListeTraitementsPage {
 
   traitements;
+  date_derniere_prise: string;
+  current_traitement: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private ngZone: NgZone, private storage: Storage,
-               private fonctionCommunes : FonctionsCommunesProvider, private notificationsProvider: NotificationsProvider, private gestionTraitement : GestionTraitementProvider) {
+    private fonctionCommunes: FonctionsCommunesProvider, private notificationsProvider: NotificationsProvider, private gestionTraitement: GestionTraitementProvider) {
     //console.log(this.traitements);
-      //aller chercher en local
-      this.storage.get('TransAgenda_traitements').then((liste) => {
-        if (liste && liste.length > 0) {
-          this.traitements = liste;
-          console.log(this.traitements);
-          //TEST
-          //this.notifService.createNotifications(this.traitements[0]);
-        }
-      }).catch((err) => {
-        console.log('erreur get liste traitements local', err);
-      });
+
+    //aller chercher en local
+    this.storage.get('TransAgenda_traitements').then((liste) => {
+      if (liste && liste.length > 0) {
+        this.traitements = liste;
+        console.log(this.traitements);
+        //TEST
+        //this.notifService.createNotifications(this.traitements[0]);
+      }
+    }).catch((err) => {
+      console.log('erreur get liste traitements local', err);
+    });
   }
 
   ionViewDidLoad() {
@@ -37,24 +40,33 @@ export class ListeTraitementsPage {
 
   openDetails(traitement) {
     console.log(traitement);
-    this.navCtrl.push(DetailTraitementPage, {traitement: traitement});
+    this.navCtrl.push(DetailTraitementPage, { traitement: traitement });
   }
 
-  updateTraitement(traitement) {
+  @ViewChild('datePicker') datePicker;
+  showDatePicker(traitement) {
+    this.current_traitement = traitement;
+    this.datePicker.open();
+  }
+
+  updateTraitement() {
+    // récupération de la dernière valeur date_derniere_prise
+    this.current_traitement.last_date = this.date_derniere_prise;
 
     this.ngZone.run(() => {
       // mise à jour de la zone pour la prochaine prise
-      this.gestionTraitement.updateNextZone(traitement);
+      this.gestionTraitement.updateNextZone(this.current_traitement);
 
       // mise à jour de la date de la prochaine prise et de la derniere
-      this.gestionTraitement.updateDates(traitement);
+      this.gestionTraitement.updateDates(this.current_traitement);
+      console.log("last_date = " + this.current_traitement.last_date + "  next_date = " + this.current_traitement.next_date);
 
       // mise à jour des rappels programmés
-      this.notificationsProvider.scheduleAllRappels(traitement);
-   
-   });
-}
-    
+      this.notificationsProvider.scheduleAllRappels(this.current_traitement);
+    });
+
+  }
+
 
   new() {
     this.navCtrl.setRoot(NewTraitementPage)
--- src/providers/gestion-traitement/gestion-traitement.ts
+++ src/providers/gestion-traitement/gestion-traitement.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Storage} from '@ionic/storage';
+import { Storage } from '@ionic/storage';
 import { FonctionsCommunesProvider } from "../../providers/fonctions-communes/fonctions-communes";
 
 
@@ -9,27 +9,27 @@ import { FonctionsCommunesProvider } from "../../providers/fonctions-communes/fo
 @Injectable()
 export class GestionTraitementProvider {
 
-  constructor(private storage: Storage, private fonctionCommunes : FonctionsCommunesProvider,) {  
+  constructor(private storage: Storage, private fonctionCommunes: FonctionsCommunesProvider, ) {
   }
-  
-  updateOneTraitementInStorage(modifiedTraitement){
+
+  updateOneTraitementInStorage(modifiedTraitement) {
     return new Promise((resolve, reject) => {
       console.log('update', modifiedTraitement);
       // Recupere la liste de traitements existante, modifie UN des traitement, enregistre liste modifiée dans storage
-      this.storage.get('TransAgenda_traitements').then((liste)=> {
-        if(liste) {
+      this.storage.get('TransAgenda_traitements').then((liste) => {
+        if (liste) {
           let originTraitementIndex = liste.findIndex(traitement => traitement.id === modifiedTraitement.id);
           console.log('index ancien', originTraitementIndex);
-          if(originTraitementIndex!==-1)
+          if (originTraitementIndex !== -1)
             liste[originTraitementIndex] = modifiedTraitement;
           else //non trouvé dans la liste, pas normal
-            console.log('erreur impossible de retrouver traitement '+modifiedTraitement.id);
-        
-        // Si la liste est nulle, on la créé... utile ? de tte façon y'a un souci.  
+            console.log('erreur impossible de retrouver traitement ' + modifiedTraitement.id);
+
+          // Si la liste est nulle, on la créé... utile ? de tte façon y'a un souci.  
         } else {
-            liste = [modifiedTraitement];
+          liste = [modifiedTraitement];
         }
-  
+
         // Remplace liste dans storage
         this.storage.set('TransAgenda_traitements', liste).then(() => {
           resolve(true);
@@ -40,71 +40,69 @@ export class GestionTraitementProvider {
       }).catch((err) => {
         console.log('erreur gestion-traitement updateOneTraitementInStorage get liste traitements local', err);
         reject(err);
-      });  
+      });
     })
   }
 
 
-  updateNextZone(traitement){
-        if(!traitement.zones || traitement.zones.length < 1)
-        {
-          return;
-        }
-
-        if(traitement.zones.length >= 2)
-        {
-            let futureNextZoneIndex;
-            let ancienneNextZoneIndex = traitement.zones.findIndex(zone => zone === traitement.next_zone);
-            // Si on est pas a la fin de la liste des zones existantes, on prend la prochaine
-            if (ancienneNextZoneIndex +1 < traitement.zones.length)
-            {
-                futureNextZoneIndex = ancienneNextZoneIndex +1;
-            }
-            else  // Sinon, on recommence du début
-            {
-              futureNextZoneIndex = 0;
-            }
-
-            traitement.next_zone = traitement.zones[futureNextZoneIndex];
-            this.updateOneTraitementInStorage(traitement);
-        }
+  updateNextZone(traitement) {
+    if (!traitement.zones || traitement.zones.length < 1) {
+      return;
     }
 
-    supprimerTraitement(traitement) {
-      return new Promise((resolve, reject) => {
-        this.storage.get('TransAgenda_traitements').then((liste)=> {
-          if(liste) {
-            let traitementIndex = liste.findIndex(tt => tt.id === traitement.id);
-            if(traitementIndex!==-1) {
-              liste.splice(traitementIndex, 1);
-              this.storage.set('TransAgenda_traitements', liste).then((res) => {
-                resolve(true);
-              })
-            } else {
-              reject(false);
-            }
+    if (traitement.zones.length >= 2) {
+      let futureNextZoneIndex;
+      let ancienneNextZoneIndex = traitement.zones.findIndex(zone => zone === traitement.next_zone);
+      // Si on est pas a la fin de la liste des zones existantes, on prend la prochaine
+      if (ancienneNextZoneIndex + 1 < traitement.zones.length) {
+        futureNextZoneIndex = ancienneNextZoneIndex + 1;
+      }
+      else  // Sinon, on recommence du début
+      {
+        futureNextZoneIndex = 0;
+      }
+
+      traitement.next_zone = traitement.zones[futureNextZoneIndex];
+      this.updateOneTraitementInStorage(traitement);
+    }
+  }
+
+  supprimerTraitement(traitement) {
+    return new Promise((resolve, reject) => {
+      this.storage.get('TransAgenda_traitements').then((liste) => {
+        if (liste) {
+          let traitementIndex = liste.findIndex(tt => tt.id === traitement.id);
+          if (traitementIndex !== -1) {
+            liste.splice(traitementIndex, 1);
+            this.storage.set('TransAgenda_traitements', liste).then((res) => {
+              resolve(true);
+            })
           } else {
             reject(false);
           }
-        });
-      })
-    }
+        } else {
+          reject(false);
+        }
+      });
+    })
+  }
 
+
+  updateDates(traitement) {
+    traitement.last_date = this.fonctionCommunes.toDateFormat(traitement.last_date);
+    traitement.next_date = this.fonctionCommunes.toPrintDateFormat(this.getCalculatedNextDate(traitement.last_date, traitement.frequence));
     
-    updateDates(traitement){
-      traitement.last_date = this.fonctionCommunes.toPrintDateFormat(new Date());
-      traitement.next_date = this.fonctionCommunes.toPrintDateFormat(this.getCalculatedNextDate(traitement));
-      this.updateOneTraitementInStorage(traitement);
-    }
+    traitement.last_date = this.fonctionCommunes.toPrintDateFormat(traitement.last_date);
+    this.updateOneTraitementInStorage(traitement);
+  }
 
-    initNextDate(traitement){
-      traitement.next_date = this.fonctionCommunes.toPrintDateFormat(traitement['start_date']);
-    }
+  initNextDate(traitement) {
+    traitement.next_date = this.fonctionCommunes.toPrintDateFormat(traitement['start_date']);
+  }
 
-    private getCalculatedNextDate(traitement) {
-      let last = this.fonctionCommunes.toDateFormat(traitement.next_date);
-      var nextDate = new Date(last.setTime(new Date().getTime() + traitement.frequence * 86400000));// version calcul a partir d'aujourd'hui
-      //last.getTime() + traitement.frequence * 86400000 ));  //version calcul a partir derniere date de prise
-      return nextDate;
-    }  
+  private getCalculatedNextDate(lastDate : Date, frequence : any) {
+    //var nextDate = new Date(last.setTime(new Date().getTime() + traitement.frequence * 86400000));// version calcul a partir d'aujourd'hui
+    var nextDate = new Date(lastDate.getTime() + frequence * 86400000);  //version calcul a partir derniere date de prise
+    return nextDate;
+  }
 }
